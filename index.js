2c225a6111ef247d87b6806e42c72cbddb3c3490
Luis <luisbrandino.contato@gmail.com>
2020-09-01 21:38:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+require('dotenv').config();
+
+const client = new Discord.Client();
+
+const prefix = "!";
+
+client.on("message", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (command === "ping") {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+  }
+});
+
+client.login(process.env.token);
